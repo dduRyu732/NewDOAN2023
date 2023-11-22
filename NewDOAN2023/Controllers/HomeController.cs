3795cadff6122a2b0defc780c56258614340bba1
Nguyễn Thành Phát <22dh112690@st.huflit.edu.vn>
2023-11-22 20:34:56 +0700

    Add project files.

---
+++ NewDOAN2023/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NewDOAN2023.Models;
+using System.IO;
+
+
+namespace LTW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DBSoTayEntities db = new DBSoTayEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult TrangChu()
+        {
+            var products = db.Products.Include(p => p.Category);
+            return View(products.ToList());
+        }    
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Probycate1()
+        {
+            List<Product> probycate = db.Products.ToList();
+            return View(probycate);
+
+        }
+        public ActionResult Set()
+        {
+            var products = db.Products.Include(p => p.Category).Include(p => p.ProDetails);
+
+            return View(products.ToList());
+        }
+        public ActionResult Bia()
+        {
+            var products = db.Products.Include(p => p.Category).Include(p => p.ProDetails);
+            return View(products.ToList());
+        }
+        public ActionResult Ruot()
+        {
+            var products = db.Products.Include(p => p.Category).Include(p => p.ProDetails);
+            return View(products.ToList());
+        }
+        public ActionResult Tap()
+        {
+            var products = db.Products.Include(p => p.Category).Include(p => p.ProDetails);
+            return View(products.ToList());
+        }
+        public ActionResult Phu()
+        {
+            var products = db.Products.Include(p => p.Category).Include(p => p.ProDetails);
+            return View(products.ToList());
+        }
+        public ActionResult ProductDetailView(int? id)
+        {
+
+            List<ProDetail> proDetails = db.ProDetails.Include(p => p.Product).Where(p => p.RemainQuantity > 0).ToList();
+            return View(proDetails);
+        }
+    }
+}
---
+++ NewDOAN2023/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewDOAN2023.MvcApplication" Language="C#" %>
